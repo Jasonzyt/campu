87f20c9a354a105cb5e800ae0a5127ba604f1a0a
Jasonzyt <66063199+Jasonzyt@users.noreply.github.com>
2025-09-28 01:08:29 +0800

    feat: add qsl components

---
+++ app/components/ArrowWithLabel.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="relative flex items-center box-content">
+    <span class="absolute w-full text-2xl text-center bottom-2" v-html="label"></span>
+    <Icon name="my:long-arrow" :style="{ width, height, color }" />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  label: {
+    type: String,
+    default: "",
+  },
+  width: {
+    type: String,
+    default: "350px",
+  },
+  height: {
+    type: String,
+    default: "24px",
+  },
+  color: {
+    type: String,
+    default: "#000000",
+  },
+});
+</script>
---
+++ app/components/QslTableCell.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="rounded-xl bg-elevated py-2 px-4 flex flex-col text-3xl">
+    <div v-if="titleTopLeft || titleTopRight" class="flex mt-1">
+      <span v-if="titleTopLeft" class="text-xl font-bold text-muted">{{ titleTopLeft }}</span>
+      <div v-if="titleTopRight" class="grow" />
+      <span v-if="titleTopRight" class="text-xl font-bold text-muted">{{ titleTopRight }}</span>
+    </div>
+    <span class="flex-1 flex" :class="`justify-${horizontalAlign} items-${verticalAlign}`"><slot /></span>
+    <div v-if="titleBottomLeft || titleBottomRight" class="flex">
+      <span v-if="titleBottomLeft" class="text-xl font-bold text-muted">{{ titleBottomLeft }}</span>
+      <div v-if="titleBottomRight" class="grow" />
+      <span v-if="titleBottomRight" class="text-xl font-bold text-muted">{{ titleBottomRight }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  titleTopLeft: {
+    type: String,
+  },
+  titleTopRight: {
+    type: String,
+  },
+  titleBottomLeft: {
+    type: String,
+  },
+  titleBottomRight: {
+    type: String,
+  },
+  verticalAlign: {
+    type: String,
+    default: "center",
+    validator: (value: string) => ["left", "center", "right", "normal"].includes(value),
+  },
+  horizontalAlign: {
+    type: String,
+    default: "center",
+    validator: (value: string) => ["left", "center", "right", "normal"].includes(value),
+  },
+});
+</script>
